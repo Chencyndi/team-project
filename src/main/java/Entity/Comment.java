6e514cfbc3442327ec7247929f050bcac9725f3d
Felix Tang <felix.tang@mail.utoronto.ca>
2025-11-10 14:04:52 -0500

    complete 4 entity class

--- src/main/java/Entity/Comment.java
+++ src/main/java/Entity/Comment.java
@@ -1,4 +1,27 @@
 package Entity;
 
 public class Comment {
+    private final String commentID;
+    private final User commenter;
+    private final Comment parentComment;
+    private final String text;
+
+    public Comment(String commentID, User commenter, Comment parentComment, String text) {
+        this.commentID = commentID;
+        this.commenter = commenter;
+        this.parentComment = parentComment;
+        this.text = text;
+    }
+    public  String getCommentID() {
+        return commentID;
+    }
+    public User getCommenter() {
+        return commenter;
+    }
+    public Comment getParentComment() {
+        return parentComment;
+    }
+    public String getText() {
+        return text;
+    }
 }
