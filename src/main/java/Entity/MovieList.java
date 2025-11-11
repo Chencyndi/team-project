d494138f6fdd3b2dd584324f821febde571fbdb5
RianMehta21 <rianmehta20@gmail.com>
2025-11-10 19:36:31 -0500

    rename files

--- src/main/java/Entity/MovieList.java
+++ src/main/java/Entity/MovieList.java
@@ -2,10 +2,10 @@ package Entity;
 
 import java.util.List;
 
-public class WatchList {
+public class MovieList {
     private final List<Movie> list;
 
-    public WatchList(List<Movie> list) {
+    public MovieList(List<Movie> list) {
         this.list = list;
     }
     public List<Movie> getList() {
